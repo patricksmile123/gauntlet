6dd24244bc7bd1e18c1d8b300ec3f1b80376b7ab
patrick <patricktierneylever@gmail.com>
2024-08-12 18:40:04 +0100

    Updated SQL call

--- leaderboard.sql
+++ leaderboard.sql
@@ -1,14 +1,17 @@
-WITH scored_games (game_id, user_id, score, start_time, end_time) as (
-    SELECT g.game_id, g.user_id, count(1) as score, g.start_time, g.end_time
-    from games g 
-    INNER JOIN wordle_guess w on w.game_id = g.game_id
+WITH scored_games AS (
+    SELECT g.game_id, g.user_id, COUNT(1) AS score, g.start_time, g.end_time
+    FROM games g 
+    INNER JOIN wordle_guess w ON w.game_id = g.game_id
     GROUP BY g.game_id, g.user_id, g.start_time, g.end_time
 )
-SELECT u.firstname, avg(g.score) as score, avg(strftime('%s', g.end_time)) - avg(strftime('%s', g.start_time)) as avg_time,
-rank() over (order by avg(g.score) asc, avg(strftime('%s', g.end_time)) - avg(strftime('%s', g.start_time)) asc) as rank
+SELECT u.firstname,
+       AVG(g.score) AS score,
+       ROUND(AVG(EXTRACT(EPOCH FROM g.end_time) - EXTRACT(EPOCH FROM g.start_time)), 2) AS avg_time,
+       RANK() OVER (
+           ORDER BY AVG(g.score) ASC, 
+                    ROUND(AVG(EXTRACT(EPOCH FROM g.end_time) - EXTRACT(EPOCH FROM g.start_time)), 2) ASC
+       ) AS rank
 FROM users u
-INNER JOIN scored_games g on (
-	u.user_id = g.user_id
-)
-GROUP by u.firstname
-ORDER by avg(g.score), avg(strftime('%s', g.end_time)) - avg(strftime('%s', g.start_time))
+INNER JOIN scored_games g ON u.user_id = g.user_id
+GROUP BY u.firstname
+ORDER BY score, avg_time
