9c2b7532024989b4b9f73e39dccbb6c1def3b10b
patrick <patricktierneylever@gmail.com>
2024-08-19 21:01:20 +0100

    Added old app changes back in

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate, json, useSearchParams, useLocation } from 'react-router-dom';
 import Wordle from './Wordle';
+import WordleN from './WordleN';
 import './App.css';
+import Signup from './Signup';
+import Login from './Login';
 import LocationProvider from './LocationProvider';
+import Leaderboard from './Leaderboard';
+import Navbar from './Navbar';
+import Achievements from './Achievements';
 import { SnackbarProvider, VariantType, useSnackbar } from 'notistack';
 
 
@@ -21,14 +27,31 @@ function App() {
 		query = location.split("/").pop();
 	}
 	return (
+		<><Navbar user={user} setUser={setUser} location={location}/>
+		<SnackbarProvider maxSnack={10}>
 	  <div className="content">
 		<Router>
 			<Routes>
-				<Route path="*" element={<Wordle user={user} />} /> 
+			{!!(user.username) && <>
+				<Route path="/wordle" element={<Wordle user={user} />} />
+				<Route path="/achievements" element={<Achievements user={user} />} />
+				<Route path="/leaderboard" element={<Leaderboard user={user} />} />
+				<Route path="/wordleN" element={<WordleN user={user} setWordLength={setWordLength} wordLength={wordLength} uuid = {query}/>} />
+				<Route path="/shared/*" element={<Wordle user={user} />} />
+				<Route path="/sharedn/*" element={<Navigate to={"/wordleN"} />} />
+			</>}
+			{!(user.username) && <>
+				<Route path="/signup" element={<Signup user={user} />}/>
+				<Route path="/login" element={<Login user={user} setUser={setUser}/>}/>
+				<Route path="/leaderboard" element={<Leaderboard user={user} />} />
+			</>}
+			<Route path="*" element={<Navigate user={user} to={!!(user.username) ? "/wordle" : "/login"}/>} /> 
 			</Routes>
 			<LocationProvider setLocation={setLocation} />
 		</Router>
 	  </div>
+	  </SnackbarProvider>
+	  </>
 	);
   } 
 
